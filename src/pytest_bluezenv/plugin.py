24cc8cab25566f01b9a83adc4217b02664bedc80
Pauli Virtanen <pav@iki.fi>
2026-05-09 13:46:29 +0300

    plugin: add wait before copying core files, in case host is dumping
    Host kernel may take some time to dump the core files, so add delay
    before copying them.

--- src/pytest_bluezenv/plugin.py
+++ src/pytest_bluezenv/plugin.py
@@ -2,6 +2,8 @@
 # SPDX-License-Identifier: GPL-2.0-or-later
 import os
 import re
+import time
+import fnmatch
 import shutil
 import logging
 import warnings
@@ -575,10 +577,17 @@ def _close_hosts(request, vm, name):
 
 
 def _copy_host_files(vm):
+    wait_done = False
+
     for j, h in enumerate(vm.hosts):
         path = Path(h._path).parent / f"shared-{j}"
         for f in path.iterdir():
             if f.name.startswith("test-bluezenv-"):
+                if not wait_done and fnmatch.fnmatch(f.name, "*.core"):
+                    # Add delay in case dump + transfer is not complete yet
+                    time.sleep(4)
+                    wait_done = True
+
                 shutil.copyfile(f, f.name)
                 os.unlink(f)
                 if f.name.endswith(".core"):
