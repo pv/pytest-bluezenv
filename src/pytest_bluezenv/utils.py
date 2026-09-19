7d6ed6419e63f64cfc964ac45c32ad1064e80810
Pauli Virtanen <pav@iki.fi>
2026-09-19 19:49:26 +0300

    utils: fix LogNameFilter name matching
    fnmatch.translate() produces pattern that has end of string marker.
    Strip it so we match correctly.

--- src/pytest_bluezenv/utils.py
+++ src/pytest_bluezenv/utils.py
@@ -752,7 +752,9 @@ class LogNameFilter(logging.Filter):
 
     def _re(self, name):
         pat = fnmatch.translate(name)
-        return f"{pat}$|{pat}\\."
+        if pat.endswith(r"\z"):
+            pat = pat[:-2]
+        return f"{pat}(?:\\z|\\.)"
 
     def filter(self, record):
         if self.deny is not None and self.deny.match(record.name):
